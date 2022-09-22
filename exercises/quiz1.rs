22a1be578c54a248ecba712b04742fab05de03cc
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-09-22 14:55:49 +0300

    Updated

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,13 +10,10 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-
-
 // Put your function here!
- fn calculate_price_of_apples(num_of_apples:i32) -> i32 {
-    if num_of_apples > 40 { num_of_apples} else { num_of_apples *2}
- }
-
+fn calculate_price_of_apples(num_of_apples: i32) -> i32 {
+    if num_of_apples > 40 {num_of_apples} else {num_of_apples * 2}
+}
 
 // Don't modify this function!
 #[test]
--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -2,7 +2,6 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
 
-
 fn main() {
     let x = 0;
     if x == 10 {
